fefa6c239c0e22a5b33ebe094f44d2effb635df4
Miguel Jimenez <migueleduardo298@hotmail.com>
2021-02-19 19:40:51 -0500

    Setup of both services: albums and logs

---
+++ logs-service/src/main/java/service/CalculatorImpl.java
@@ -0,0 +1,35 @@
+package service;
+import models.ResultData;
+
+import javax.jws.WebService;
+
+/**
+ * This class holds the implementation of the methods of our SOAP web service
+ */
+@WebService(endpointInterface = "service.Calculator")
+public class CalculatorImpl implements Calculator {
+
+	/**
+	 * Prints a simple message with input from the client.
+	 * @param content
+	 * @return
+	 */
+	@Override
+	public String sayHelloWorld(String content) {
+		return "Hello " + content + "!";
+	}
+
+	@Override
+	public double add(double num1, double num2) { return num1 + num2; }
+
+	@Override
+	public double subtract(double num1, double num2) { return num1 - num2; }
+
+	@Override
+	public double multiply(double num1, double num2) { return num1 * num2; }
+
+	@Override
+	public ResultData multiplyV2(double num1, double num2) {
+		return new ResultData(num1, num2);
+	}
+}
