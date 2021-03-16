74b5c5d67cd7c7a1412e389f6e7b049a7519057b
Nicolas Zito <nicolasazito@gmail.com>
2021-03-16 11:08:50 -0400

    Updated AlbumGateway for PR
    Push for PR.
    Insert and Update now have cover art functionality.
    Find & Find All still need to be worked on, to read blob from db.

--- albums-service/src/main/java/db/AlbumGateway.java
+++ albums-service/src/main/java/db/AlbumGateway.java
@@ -8,15 +8,7 @@ import java.io.InputStream;
 import java.sql.*;
 import java.util.ArrayList;
 
-//International Standard Recording Code (ISRC): unique
-//Title
-//Content description: optional
-//Release year
-//Artist's first and last names^
-//Cover image: optional^^
-
 //Using Table Data Gateway, gateway handles all SQL for business layer.
-
 public class AlbumGateway {
     static Connection connect = null;
 
@@ -49,7 +41,6 @@ public class AlbumGateway {
         }
         return null;
     }
-
     //Returns an ArrayList of album objects
     public ArrayList <Album> findAll(){
         connect = DBConnect.connect();
@@ -67,15 +58,6 @@ public class AlbumGateway {
                 int releaseYear = table.getInt("year");
                 String firstname = table.getString("firstname");
                 String lastname = table.getString("lastname");
-//                InputStream inStream = table.getBinaryStream("coverart");
-//                if (inStream != null) {
-//                    File fileArt = new File("/Users/zito/Desktop/ca.jpeg");
-//                    FileOutputStream fileOut = new FileOutputStream(fileArt);
-//                    byte[] bytes = new byte[1024];
-//                    while (inStream.read(bytes) > 0) {
-//                        fileOut.write(bytes);
-//                    }
-//                }
                 Album album = new Album(isrc,title,description,releaseYear, new Artist(firstname,lastname), new Cover()); //
                 albums.add(album);
             }
@@ -94,7 +76,35 @@ public class AlbumGateway {
         }
         return null;
     }
-    public void insert(String isrc, String title, String description, int releaseYear, Artist artist, String pathname){
+    public void insert(String isrc, String title, String description, int releaseYear, Artist artist){
+        connect = DBConnect.connect();
+        try{
+
+            String insertStr = "INSERT INTO Albums (isrc, title, description, year, firstname, lastname) VALUES(?,?,?,?,?,?)";
+            PreparedStatement insertStatement = connect.prepareStatement(insertStr);
+            insertStatement.setString(1, isrc);
+            insertStatement.setString(2, title);
+            insertStatement.setString(3, description);
+            insertStatement.setInt(4, releaseYear);
+            insertStatement.setString(5, artist.getFirstName());
+            insertStatement.setString(6, artist.getLastName());
+            insertStatement.executeUpdate();
+            System.out.println(insertStr);
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        finally{
+            try {
+                DBConnect.disconnect();
+            }
+            catch (SQLException e){
+                throw new RuntimeException("ERROR: Failed to connect.", e);
+            }
+        }
+    }
+    public void insertWithArt(String isrc, String title, String description, int releaseYear, Artist artist, String pathname){
         connect = DBConnect.connect();
         try{
 
@@ -125,7 +135,34 @@ public class AlbumGateway {
             }
         }
     }
-    public void update(String isrc, String title, String description, int releaseYear, Artist artist, String pathname){
+    public void update(String isrc, String title, String description, int releaseYear, Artist artist){
+        connect = DBConnect.connect();
+        try{
+            String updateStr = "UPDATE Albums SET isrc=?, title=?, description=?, year=?, firstname=?, lastname=? WHERE isrc=?;";
+            PreparedStatement updateStatement = connect.prepareStatement(updateStr);
+            updateStatement.setString(1, isrc);
+            updateStatement.setString(2, title);
+            updateStatement.setString(3, description);
+            updateStatement.setInt(4, releaseYear);
+            updateStatement.setString(5, artist.getFirstName());
+            updateStatement.setString(6, artist.getLastName());
+            updateStatement.setString(7, isrc);
+            updateStatement.executeUpdate();
+            System.out.println(updateStr);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        finally{
+            try {
+                DBConnect.disconnect();
+            }
+            catch (SQLException e){
+                throw new RuntimeException("ERROR: Failed to connect.", e);
+            }
+        }
+    }
+    public void updateWithArt(String isrc, String title, String description, int releaseYear, Artist artist, String pathname){
         connect = DBConnect.connect();
         try{
             String updateStr = "UPDATE Albums SET isrc=?, title=?, description=?, year=?, firstname=?, lastname=?, coverart=? WHERE isrc=?;";
@@ -178,15 +215,15 @@ public class AlbumGateway {
     // For Testing
     public static void main(String[] args) {
         //Declaring AlbumGateway Object
-        AlbumGateway ag = new AlbumGateway();
+        //AlbumGateway ag = new AlbumGateway();
         //Find Album by ISRC
-        //System.out.println(ag.find(1).getReleaseYear());
+        //System.out.println(ag.find("1").getReleaseYear());
         //Find All Albums
-        System.out.println(ag.findAll().get(4).getCover().getFile());
+        //System.out.println(ag.findAll().get(4).getCover().getFile());
         //Insert Values into Albums Table
-        //ag.insert("5","title5","Description 5", 5555, new Artist("firstfive","lastfive"), "/Users/zito/Desktop/sampleCoverArt.jpeg");
+        //ag.insertWithArt("5","title5","Description 5", 5555, new Artist("firstfive","lastfive"), "/Users/username/Desktop/sampleCoverArt.jpeg");
         //Update Values of a specific Album in the Albums Table
-        //ag.update("5","title5","Description 5", 5555, new Artist("NEWfirstfive","NEWlastfive"), "/Users/zito/Desktop/sampleCoverArt.jpeg");
+        //ag.updateWithArt("5","title5","Description 5", 5555, new Artist("NEWfirstfive","NEWlastfive"), "/Users/username/Desktop/sampleCoverArt.jpeg");
         //Delete specific Album using ISRC from Albums Table
         //ag.delete("5");
     }
