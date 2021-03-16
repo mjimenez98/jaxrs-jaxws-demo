a40af8f949ba9b470265671fbcffb7ab0c5fd45e
Miguel Jimenez <migueleduardo298@hotmail.com>
2021-03-16 00:24:55 -0400

    Template for editing an album

--- client/src/main/java/client/AlbumsController.java
+++ client/src/main/java/client/AlbumsController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
@@ -23,8 +24,12 @@ public class AlbumsController {
         model.addAttribute("search", new Search());
 
         List<Album> albums = new ArrayList<>();
-        albums.add(new Album("1", "2", "3", 4, new Artist("A", "B"), new Cover(null, null)));
-        albums.add(new Album("5", "6", "7", 8, new Artist("A", "B"), new Cover(null, null)));
+        albums.add(new Album("1", "2", "3", 4,
+                new Artist("A", "B"),
+                new Cover(null, null)));
+        albums.add(new Album("5", "6", "7", 8,
+                new Artist("A", "B"),
+                new Cover(null, null)));
 
         model.addAttribute("albums", albums);
         return "albums";
@@ -40,7 +45,23 @@ public class AlbumsController {
 
     @PostMapping("/albums/new")
     public String newAlbumSubmit(@ModelAttribute Album album, Model model) {
-        System.out.println("WORKING");
+        return "redirect:/albums";
+    }
+
+//    EDIT ALBUM
+
+    @GetMapping("/albums/edit/{isrc}")
+    public String editAlbum(@PathVariable("isrc") String isrc, Model model) {
+        model.addAttribute("album", new Album("1", "2", "3", 4,
+                        new Artist("A", "B"),
+                        new Cover(null, null)));
+        model.addAttribute("isrc", isrc);
+
+        return "edit";
+    }
+
+    @PostMapping("/albums/edit/{isrc}")
+    public String editAlbumSubmit(@ModelAttribute Album album, Model model) {
         return "redirect:/albums";
     }
 }
